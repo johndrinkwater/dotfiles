2f63da191c6097b08a47275bbf80172db599b6fd
John Drinkwater <john@nextraweb.com>
2015-05-18 11:15:01 +0100

    [osx] Add my current karabiner config

---
+++ osx/karabiner-config.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.consumer_to_fkeys_f5 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f1 1
+/bin/echo -n .
+$cli set remap.controlL2fn 1
+/bin/echo -n .
+$cli set remap.eject2forwarddelete_retain_fnEject 1
+/bin/echo -n .
+$cli set remap.fnF1_F12_to_functional 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f10 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f7 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f3 1
+/bin/echo -n .
+$cli set remap.uk_backslash2hash 1
+/bin/echo -n .
+$cli set remap.uk_swap_at_doublequote 1
+/bin/echo -n .
+$cli set remap.fn2controlL 1
+/bin/echo -n .
+$cli set remap.uk_saneuklayout2 1
+/bin/echo -n .
+/bin/echo
